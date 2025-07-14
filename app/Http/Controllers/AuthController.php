875c89bd738da5f48c903452fbf99fc25b16fe13
tsumich <lomakinadiana2016@gmail.com>
2025-07-15 01:58:21 +0700

    28 video

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,31 +4,32 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        return view('auth.create');
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('auth.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'email'    => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $credentials = $request->only('email', 'password');
+        $remember = $request->filled('remember');
+
+        if(Auth::attempt($credentials, $remember)){
+            return redirect()->intended('/');
+        }else{
+            return redirect()->back()
+            ->with('error', 'Invalid');
+        }
     }
 
     /**
@@ -58,8 +59,12 @@ class AuthController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy()
     {
-        //
+        Auth::logout();
+
+        request()->session()->invalidate();
+        request()->session()->regenerateToken();
+        return redirect('/');
     }
 }
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,6 +11,31 @@
     </head>
     <body class="mx-auto mt-10 max-w-2xl bg-gradient-to-r from-blue-400 via-purple-400
                to-pink-200 text-slate-700">
+        <nav class="mb-8 flex justify-between text-lg font-medium">
+            <ul class="flex space-x-2">
+                <li>
+                    <a href="{{route('jobs.index')}}">Home</a>
+                </li>
+            </ul>
+            <ul class="flex space-x-2">
+                @auth
+                   <li>
+                        {{auth()->user()->name ?? 'Anynomus'}}
+                   </li>
+                   <li>
+                        <form action="{{route('auth.destroy')}}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button>Выйти</button>
+                        </form>
+                   </li>
+                @else
+                   <li>
+                        <a href="{{route('auth.create')}}">Войти</a>
+                   </li>
+                @endauth
+            </ul>
+        </nav>
         {{ $slot }}
     </body>
 </html>
